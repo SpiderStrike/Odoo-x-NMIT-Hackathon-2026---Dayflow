d2bcd64914a75a2f21a9eec84df54470e08f318e
SpiderStrike <98522941+SpiderStrike@users.noreply.github.com>
2026-08-22 12:34:52 +0530

    Add employee user relationship

--- addons/dayflow_core/models/user.py
+++ addons/dayflow_core/models/user.py
@@ -1,21 +1,12 @@
-from odoo import api, models
+from odoo import fields, models
 
 
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    @api.model
-    def create_dayflow_user(self, employee):
-        if not employee.user_id:
-            user = self.create({
-                "name": employee.name,
-                "login": employee.dayflow_employee_code,
-                "email": employee.work_email,
-                "employee_id": employee.id,
-                "groups_id": [(6, 0, [
-                    self.env.ref("dayflow_core.group_dayflow_employee").id
-                ])],
-            })
-            employee.user_id = user.id
-            return user
-        return employee.user_id
+    employee_id = fields.Many2one(
+        "hr.employee",
+        string="Employee",
+        readonly=True,
+        copy=False,
+    )
